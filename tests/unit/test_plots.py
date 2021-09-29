ac374fbe3850a17ac4161376e6e4ee073d6240d4
Tom Tranter <t.g.tranter@gmail.com>
2021-09-29 18:37:56 +0100

    test ghostscript again

--- tests/unit/test_plots.py
+++ tests/unit/test_plots.py
@@ -9,13 +9,13 @@ class plotsTest(unittest.TestCase):
         pass
 
     def test_draw_circuit(self):
-        # net = lp.setup_circuit(Np=3, Ns=1, Rb=1e-4,
-        #                        Rc=1e-2, Ri=5e-2, V=3.2, I=80.0)
-        # lp.draw_circuit(net)
-        # plt.close('all')
+        net = lp.setup_circuit(Np=3, Ns=1, Rb=1e-4,
+                                Rc=1e-2, Ri=5e-2, V=3.2, I=80.0)
+        lp.draw_circuit(net)
+        plt.close('all')
         
         # cannot get this working on github actions
-        pass
+        # pass
 
 if __name__ == '__main__':
     unittest.main()
