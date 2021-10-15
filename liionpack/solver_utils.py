2b148c4f14e129b347eaba06cf3dcf5fa48e90ca
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-10-15 15:53:41 -0400

    Work on Dask functions

--- liionpack/solver_utils.py
+++ liionpack/solver_utils.py
@@ -11,6 +11,7 @@ import time as ticker
 import liionpack as lp
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from dask.distributed import Client
 
 
 def _serial_step(model, solutions, inputs_dict, integrator, variables, t_eval):
@@ -367,15 +368,60 @@ def solve(netlist=None, parameter_values=None, experiment=None,
     return all_output
 
 
+def _serial_step_dask(model, solutions, inputs_dict, integrator, variables, t_eval):
+    """
+    Internal function that uses Dask to process the model for one timestep in
+    a serial way.
+    """
+    len_rhs = model.concatenated_rhs.size
+    N = len(solutions)
+    t_min = 0.0
+    timer = pybamm.Timer()
+
+    sol = []
+    var_eval = []
+
+    for k in range(N):
+
+        if solutions[k] is None:
+            # First pass
+            x0 = model.y0[:len_rhs]
+            z0 = model.y0[len_rhs:]
+        else:
+            x0 = solutions[k].y[:len_rhs, -1]
+            z0 = solutions[k].y[len_rhs:, -1]
+
+        temp = inputs_dict[k]
+        inputs = casadi.vertcat(*[x for x in temp.values()]+[t_min])
+        ninputs = len(temp.values())
+
+        # Call the integrator once, with the grid
+        casadi_sol = integrator(x0=x0, z0=z0, p=inputs)
+        y_sol = casadi_sol["xf"]
+        xend = y_sol[:, -1]
+
+        sol.append(pybamm.Solution(t_eval, y_sol, model, inputs_dict[k]))
+        var_eval.append(variables(0, xend, 0, inputs[0:ninputs]))
+
+        integration_time = timer.time()
+        sol[-1].integration_time = integration_time
+
+    return sol, casadi.horzcat(*var_eval)
+
+
 def solve_dask(
-        netlist=None, parameter_values=None, protocol=None, dt=10,
-        I_init=1.0, htc=None, initial_soc=0.5, nproc=12, output_variables=None):
+        netlist=None, parameter_values=None, protocol=None, dt=10, I_init=1.0,
+        htc=None, initial_soc=0.5, nproc=12, output_variables=None):
     """
     Solves a battery pack simulation where Dask is used to solve the PyBaMM
     battery cell models in parallel.
     """
-    if None in (netlist, parameter_values, protocol):
+    if netlist is None or parameter_values is None or protocol is None:
         raise Exception('Please supply a netlist, paramater_values, and protocol')
+
+    # Setup client to use Dask distributed scheduler
+    client = Client()
+    print(client)
 
     # Get netlist indices for resistors, voltage sources, current sources
     Ri_map = netlist['desc'].str.find('Ri') > -1
@@ -386,5 +432,109 @@ def solve_dask(
     Nspm = sum(V_map)
     Nsteps = len(protocol)
 
-    # more coming soon...
-    return 1
+    # Solve the circuit to initialize the electrochemical cell models
+    V_node, I_batt = lp.solve_circuit(netlist)
+
+    # Create the simulation object with initial conditions
+    sim = lp.create_simulation(parameter_values, make_inputs=True)
+    lp.update_init_conc(sim, SoC=initial_soc)
+
+    # Get cut-off voltages
+    v_cut_lower = parameter_values['Lower voltage cut-off [V]']
+    v_cut_higher = parameter_values['Upper voltage cut-off [V]']
+
+    # Simulation output variables calculated at each step for each battery.
+    # Must be a 0D variable i.e. battery wide volume average or X-averaged
+    # for 1D model.
+    variable_names = ['Terminal voltage [V]',
+                      'Measured battery open circuit voltage [V]',
+                      'Local ECM resistance [Ohm]']
+
+    if output_variables is not None:
+        for out in output_variables:
+            if out not in variable_names:
+                variable_names.append(out)
+
+    # Number of variables associated with the simulation
+    Nvar = len(variable_names)
+
+    # Storage variables for simulation data
+    shm_i_app = np.zeros([Nsteps, Nspm], dtype=float)
+    shm_Ri = np.zeros([Nsteps, Nspm], dtype=float)
+    output = np.zeros([Nvar, Nsteps, Nspm], dtype=float)
+
+    # Initialize currents in battery models
+    shm_i_app[0, :] = I_batt * -1
+
+    # Initialize other variables
+    time = 0
+    end_time = dt * Nsteps
+    step_solutions = [None] * Nspm
+    V_terminal = []
+    record_times = []
+
+    # Create Casadi objects
+    integrator, variables_fn, t_eval = _create_casadi_objects(
+        I_init, htc[0], sim, dt, Nspm, nproc, variable_names, mapped=False)
+
+    # Time at which solver begins solving for each time step
+    tic = ticker.time()
+
+    # Calculate solutions for each time step
+    for step in range(Nsteps):
+
+        inputs = lp.build_inputs_dict(shm_i_app[step, :], htc)
+
+        step_solutions, var_eval = _serial_step_dask(
+            sim.built_model, step_solutions, inputs, integrator, variables_fn, t_eval)
+
+        output[:, step, :] = var_eval
+        time += dt
+
+        # Calculate internal resistance and update netlist
+        temp_v = output[0, step, :]
+        temp_ocv = output[1, step, :]
+        temp_Ri = np.abs(output[2, step, :])
+        shm_Ri[step, :] = temp_Ri
+
+        netlist.loc[V_map, ('value')] = temp_ocv
+        netlist.loc[Ri_map, ('value')] = temp_Ri
+        netlist.loc[I_map, ('value')] = protocol[step]
+
+        # Check lower and upper voltage limits
+        if np.any(temp_v < v_cut_lower):
+            print('Low V limit reached')
+            break
+
+        if np.any(temp_v > v_cut_higher):
+            print('High V limit reached')
+            break
+
+        # DESCRIPTION
+        if time <= end_time:
+            record_times.append(time)
+            V_node, I_batt = lp.solve_circuit(netlist)
+            V_terminal.append(V_node.max())
+
+        if time < end_time:
+            shm_i_app[step+1, :] = I_batt[:] * -1
+
+    # Assign all output results
+    all_output = {}
+    all_output['Time [s]'] = np.asarray(record_times)
+    all_output['Pack current [A]'] = np.asarray(protocol[:step+1])
+    all_output['Pack terminal voltage [V]'] = np.asarray(V_terminal)
+    all_output['Cell current [A]'] = shm_i_app[:step+1, :]
+
+    for j in range(Nvar):
+        all_output[variable_names[j]] = output[j, :step+1, :]
+
+    # Elapsed time to solve circuit
+    toc = ticker.time()
+    print(f'Solve circuit time {toc - tic:.3f} s')
+
+    # Stop the Dask distributed scheduler
+    client.close()
+    print(client)
+
+    return all_output
