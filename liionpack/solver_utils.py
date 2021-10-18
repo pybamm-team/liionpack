6098cad19418a24e8d272512c0f0b96cfe824be5
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-10-18 11:02:11 -0400

    Clean up solver_utils for flake8

--- liionpack/solver_utils.py
+++ liionpack/solver_utils.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Sep 23 10:44:31 2021
+Solver functions for battery cell models.
+"""
 
-@author: Tom
-"""
 import casadi
 import pybamm
 import numpy as np
 import time as ticker
 import liionpack as lp
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
 def _mapped_step(model, solutions, inputs_dict, integrator, variables, t_eval):
-    r'''
+    """
     Internal function to process the model for one timestep in a mapped way.
     Mapped versions of the integrator and variables functions should already
     have been made.
@@ -41,10 +38,10 @@ def _mapped_step(model, solutions, inputs_dict, integrator, variables, t_eval):
         solutions that have been stepped forward by one timestep
     var_eval : list
         evaluated variables for final state of system
-
-    '''
+    """
     len_rhs = model.concatenated_rhs.size
     N = len(solutions)
+
     if solutions[0] is None:
         # First pass
         x0 = casadi.horzcat(*[model.y0[:len_rhs] for i in range(N)])
@@ -52,40 +49,48 @@ def _mapped_step(model, solutions, inputs_dict, integrator, variables, t_eval):
     else:
         x0 = casadi.horzcat(*[sol.y[:len_rhs, -1] for sol in solutions])
         z0 = casadi.horzcat(*[sol.y[len_rhs:, -1] for sol in solutions])
+
     # t_min = [0.0]*N
     t_min = 0.0
     inputs = []
+
     for temp in inputs_dict:
         inputs.append(casadi.vertcat(*[x for x in temp.values()]+[t_min]))
+
     ninputs = len(temp.values())
     inputs = casadi.horzcat(*inputs)
+
     # p = casadi.horzcat(*zip(inputs, external_variables, [t_min]*N))
     # inputs_with_tmin = casadi.vertcat(inputs, np.asarray(t_min))
+
     # Call the integrator once, with the grid
     timer = pybamm.Timer()
-    casadi_sol = integrator(
-        x0=x0, z0=z0, p=inputs
-    )
-    integration_time = timer.time()
-    nt = len(t_eval)
+
+    casadi_sol = integrator(x0=x0, z0=z0, p=inputs)
     xf = casadi_sol["xf"]
     # zf = casadi_sol["zf"]
+
+    nt = len(t_eval)
+    integration_time = timer.time()
     sol = []
     xend = []
+
     for i in range(N):
         start = i * nt
         y_sol = xf[:, start:start + nt]
         xend.append(y_sol[:, -1])
         # Not sure how to index into zf - need an example
-        sol.append(pybamm.Solution(t_eval, y_sol, model,
-                                   inputs_dict[i]))
+        sol.append(pybamm.Solution(t_eval, y_sol, model, inputs_dict[i]))
         sol[-1].integration_time = integration_time
+
     xend = casadi.horzcat(*xend)
     var_eval = variables(0, xend, 0, inputs[0:ninputs, :])
+
     return sol, var_eval
 
+
 def _create_casadi_objects(I_init, htc, sim, dt, Nspm, nproc, variable_names):
-    r'''
+    """
     Internal function to produce the casadi objects in their mapped form for
     parallel evaluation
 
@@ -118,25 +123,28 @@ def _create_casadi_objects(I_init, htc, sim, dt, Nspm, nproc, variable_names):
         evaluates the simulation and output variables. see casadi function
     t_eval : float array of times to evaluate
         times to evaluate in a single step, starting at zero for each step
-
-    '''
+    """
     inputs = {"Current": I_init,
               "Total heat transfer coefficient [W.m-2.K-1]": htc}
+
     solver = sim.solver
+
     # solve model for 1 second to initialise the circuit
     t_eval = np.linspace(0, 1, 2)
+
     # Initial solution - this builds the model behind the scenes
-    sol_init = sim.solve(t_eval, inputs=inputs)
+    # sol_init = sim.solve(t_eval, inputs=inputs)
+
     # step model
     # Code to create mapped integrator
     t_eval = np.linspace(0, dt, 11)
     t_eval_ndim = t_eval / sim.model.timescale.evaluate()
     inp_and_ext = inputs
+
     # No external variables - Temperature solved as lumped model in pybamm
     # External variables could (and should) be used if battery thermal problem
     # Includes conduction with any other circuits or neighboring batteries
     # inp_and_ext.update(external_variables)
-    
     integrator = solver.create_integrator(sim.built_model, inputs=inp_and_ext, t_eval=t_eval_ndim)
     integrator = integrator.map(Nspm, 'thread', nproc)
 
@@ -144,9 +152,11 @@ def _create_casadi_objects(I_init, htc, sim, dt, Nspm, nproc, variable_names):
     casadi_objs = sim.built_model.export_casadi_objects(variable_names=variable_names)
     variables = casadi_objs['variables']
     t, x, z, p = casadi_objs["t"], casadi_objs["x"], casadi_objs["z"], casadi_objs["inputs"]
+
     variables_stacked = casadi.vertcat(*variables.values())
     variables_fn = casadi.Function("variables", [t, x, z, p], [variables_stacked])
     variables_fn = variables_fn.map(Nspm, 'thread', nproc)
+
     return integrator, variables_fn, t_eval
 
 
@@ -155,8 +165,8 @@ def solve(netlist=None, parameter_values=None, experiment=None,
           nproc=12,
           output_variables=None,
           ):
-    r'''
-    Solves a pack simulation
+    """
+    Solves a pack simulation.
 
     Parameters
     ----------
@@ -189,9 +199,7 @@ def solve(netlist=None, parameter_values=None, experiment=None,
     -------
     output : ndarray shape [# variable, # steps, # batteries]
         simulation output array
-
-    '''
-
+    """
     if netlist is None or parameter_values is None or experiment is None:
         raise Exception('Please supply a netlist, paramater_values, and experiment')
 
@@ -199,13 +207,13 @@ def solve(netlist=None, parameter_values=None, experiment=None,
     Ri_map = netlist['desc'].str.find('Ri') > -1
     V_map = netlist['desc'].str.find('V') > -1
     I_map = netlist['desc'].str.find('I') > -1
-    
+
     Nspm = np.sum(V_map)
 
     protocol = lp.generate_protocol_from_experiment(experiment)
     dt = experiment.period
     Nsteps = len(protocol)
-    
+
     # Solve the circuit to initialise the electrochemical models
     V_node, I_batt = lp.solve_circuit(netlist)
 
@@ -220,12 +228,15 @@ def solve(netlist=None, parameter_values=None, experiment=None,
     variable_names = ['Terminal voltage [V]',
                       'Measured battery open circuit voltage [V]',
                       'Local ECM resistance [Ohm]']
+
     if output_variables is not None:
         for out in output_variables:
             if out not in variable_names:
                 variable_names.append(out)
         # variable_names = variable_names + output_variables
+
     Nvar = len(variable_names)
+
     # Storage variables for simulation data
     shm_i_app = np.zeros([Nsteps, Nspm], dtype=float)
     shm_Ri = np.zeros([Nsteps, Nspm], dtype=float)
@@ -234,7 +245,6 @@ def solve(netlist=None, parameter_values=None, experiment=None,
     # Initialize currents in battery models
     shm_i_app[0, :] = I_batt * -1
 
-
     time = 0
     # step = 0
     end_time = dt*Nsteps
@@ -242,27 +252,25 @@ def solve(netlist=None, parameter_values=None, experiment=None,
     V_terminal = []
     record_times = []
 
-   
-    integrator, variables_fn, t_eval = _create_casadi_objects(I_init, htc[0],
-                                                              sim, dt, Nspm,
-                                                              nproc,
-                                                              variable_names)
+    integrator, variables_fn, t_eval = _create_casadi_objects(
+        I_init, htc[0], sim, dt, Nspm, nproc, variable_names)
 
- 
     sim_start_time = ticker.time()
-    for step in tqdm(range(Nsteps), desc='Solving Pack'):
-        step_solutions, var_eval = _mapped_step(sim.built_model, step_solutions,
-                                                lp.build_inputs_dict(shm_i_app[step, :], htc),
-                                                integrator, variables_fn, t_eval)
-        output[:, step, :] = var_eval
 
+    for step in tqdm(range(Nsteps), desc='Solving Pack'):
+        step_solutions, var_eval = _mapped_step(
+            sim.built_model, step_solutions,
+            lp.build_inputs_dict(shm_i_app[step, :], htc),
+            integrator, variables_fn, t_eval)
+
+        output[:, step, :] = var_eval
         time += dt
+
         # Calculate internal resistance and update netlist
         temp_v = output[0, step, :]
         temp_ocv = output[1, step, :]
         temp_Ri = np.abs(output[2, step, :])
         shm_Ri[step, :] = temp_Ri
-
 
         netlist.loc[V_map, ('value')] = temp_ocv
         netlist.loc[Ri_map, ('value')] = temp_Ri
@@ -282,15 +290,17 @@ def solve(netlist=None, parameter_values=None, experiment=None,
             V_terminal.append(V_node.max())
         if time < end_time:
             shm_i_app[step+1, :] = I_batt[:] * -1
+
     all_output = {}
     all_output['Time [s]'] = np.asarray(record_times)
     all_output['Pack current [A]'] = np.asarray(protocol[:step+1])
     all_output['Pack terminal voltage [V]'] = np.asarray(V_terminal)
     all_output['Cell current [A]'] = shm_i_app[:step+1, :]
+
     for j in range(Nvar):
         all_output[variable_names[j]] = output[j, :step+1, :]
-    
+
     toc = ticker.time()
-    pybamm.logger.notice('Solve circuit time '+
-                          str(np.around(toc-sim_start_time, 3)) + 's')
+    pybamm.logger.notice(f'Solve circuit time {toc - sim_start_time:.3f} s')
+
     return all_output
