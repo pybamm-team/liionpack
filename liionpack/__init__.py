5a428136133ecc2bc60798dcede441b2d33a36c5
Tom Tranter <t.g.tranter@gmail.com>
2021-10-22 15:28:02 +0100

    black format init

--- liionpack/__init__.py
+++ liionpack/__init__.py
@@ -1,4 +1,4 @@
-r'''
+r"""
 =======
 liionpack
 =======
@@ -6,7 +6,7 @@ liionpack
 liionpack is a tool for simulating battery packs with pybamm. It can design the
 pack with a combination of batteries connected in series and parallel or can
 read a netlist.
-'''
+"""
 from .simulations import *
 from .utils import *
 from .netlist_utils import *
