4af8e12726076eddec04f2a8dd41679b63c9ee53
Tom Tranter <t.g.tranter@gmail.com>
2021-10-22 15:47:15 +0100

    black format tests

--- tests/unit/test_sim_utils.py
+++ tests/unit/test_sim_utils.py
@@ -6,19 +6,18 @@ import unittest
 
 
 class sim_utilsTest(unittest.TestCase):
-
     @classmethod
     def setUpClass(self):
         chemistry = pybamm.parameter_sets.Chen2020
         self.param = pybamm.ParameterValues(chemistry=chemistry)
-        self.fname = os.path.join(lp.INIT_FUNCS, 'init_funcs.pickle')
+        self.fname = os.path.join(lp.INIT_FUNCS, "init_funcs.pickle")
 
     def test_create_init_funcs(self):
         if os.path.isfile(self.fname):
             os.remove(self.fname)
         lp.create_init_funcs(self.param)
         assert os.path.isfile(self.fname)
-        plt.close('all')
+        plt.close("all")
 
     def test_update_init_conc(self):
         sim = lp.create_simulation(self.param)
@@ -38,5 +37,6 @@ class sim_utilsTest(unittest.TestCase):
         assert a_n < b_n
         assert a_p > b_p
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -5,31 +5,28 @@ import unittest
 
 
 class utilsTest(unittest.TestCase):
-
     def test_interp_current(self):
-        d = {'Time': [0, 10], 'Cells Total Current': [2.0, 4.0]}
+        d = {"Time": [0, 10], "Cells Total Current": [2.0, 4.0]}
         df = pd.DataFrame(data=d)
         f = lp.interp_current(df)
         assert f(5) == 3.0
 
     def test_read_cfd_data_linear(self):
         data, xv, yv, planes = lp.read_cfd_data()
-        expected = np.array([8.295482896342516,
-                             7.878096887315587])
-        T = np.ones(32)*xv[1, 1]
+        expected = np.array([8.295482896342516, 7.878096887315587])
+        T = np.ones(32) * xv[1, 1]
         Q = yv[1, 1]
         htc = lp.get_linear_htc(planes, T, Q)
         assert np.allclose(htc[:2], expected)
 
     def test_read_cfd_data_interpolated(self):
-        data, xv, yv, funcs = lp.read_cfd_data(fit='interpolated')
-        expected = np.array([8.295482896342516,
-                             7.878096887315587])
-        T = np.ones(32)*xv[1, 1]
+        data, xv, yv, funcs = lp.read_cfd_data(fit="interpolated")
+        expected = np.array([8.295482896342516, 7.878096887315587])
+        T = np.ones(32) * xv[1, 1]
         Q = yv[1, 1]
         htc = lp.get_interpolated_htc(funcs, T, Q)
         assert np.allclose(htc[:2], expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
