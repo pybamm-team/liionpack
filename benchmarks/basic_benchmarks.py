37f5466a8c1ea30642884af142be1b06be5e838e
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2021-11-08 22:54:39 +0000

    #41 add test benchmarks

---
+++ benchmarks/basic_benchmarks.py
@@ -0,0 +1,12 @@
+# Write the benchmarking functions here.
+# See "Writing benchmarks" in the asv docs for more information.
+
+import liionpack as lp
+
+
+class BasicBenchmark:
+    def setup(self):
+        self.sim = lp.create_simulation()
+
+    def time_solve_model(self):
+        BasicBenchmark.sim.solve([0, 1800])
