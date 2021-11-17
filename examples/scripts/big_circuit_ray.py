d759273dc6dffd632e14f9338b1112d1be4f1994
TomTranter <t.g.tranter@gmail.com>
2021-11-17 21:37:51 +0000

    Update big_circuit_ray.py

--- examples/scripts/big_circuit_ray.py
+++ examples/scripts/big_circuit_ray.py
@@ -11,14 +11,14 @@ import time as ticker
 
 
 print('Initializing Ray')
-ray.init(dashboard_port=8080)
+ray.init()
 print('Ray initialization Complete')
 
 plt.close("all")
 lp.logger.setLevel("NOTICE")
 
-Np = 64
-Ns = 100
+Np = 32
+Ns = 10
 Nspm = Np * Ns
 I_app = Np * 2.0
 
@@ -36,7 +36,7 @@ htc = np.ones(Nspm) * 10
 # Cycling experiment
 experiment = pybamm.Experiment(
     [
-        f"Discharge at {I_app} A for 50 seconds",
+        f"Discharge at {I_app} A for 500 seconds",
         # "Rest for 100 seconds",
         # f"Charge at {I_app} A for 500 seconds",
         # "Rest for 100 seconds",
@@ -50,16 +50,23 @@ parameter_values = pybamm.ParameterValues(chemistry=chemistry)
 
 # Solve pack
 t1 = ticker.time()
-rm = lp.ray_manager.remote(Np=Np, Ns=Ns, Rb=1e-4, Rc=1e-2, Ri=5e-2, V=3.2, I=I_app)
+# rm = lp.ray_manager.remote(Np=Np, Ns=Ns, Rb=1e-4, Rc=1e-2, Ri=5e-2, V=3.2, I=I_app)
+rm = lp.ray_manager(Np=Np, Ns=Ns, Rb=1e-4, Rc=1e-2, Ri=5e-2, V=3.2, I=I_app)
 t2 = ticker.time()
-print('Ray setup time', t2 - t1)
-output = ray.get(rm.solve.remote(
+print('Ray manager setup time', t2 - t1)
+# output = ray.get(rm.solve.remote(
+#     parameter_values=parameter_values,
+#     experiment=experiment,
+#     output_variables=None,
+#     htc=htc,
+#     nproc=10,
+# ))
+output = rm.solve(
     parameter_values=parameter_values,
     experiment=experiment,
     output_variables=None,
     htc=htc,
-    nproc=10,
-))
+    nproc=32,)
 t3 = ticker.time()
 print('Ray solve time', t3 - t2)
 
