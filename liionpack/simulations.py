342b3276489b11e61b984626fa55dd2fcebcdf4e
Saransh <saransh0701@gmail.com>
2021-11-21 01:41:09 +0530

    Refactor all docstrings once

--- liionpack/simulations.py
+++ liionpack/simulations.py
@@ -9,22 +9,20 @@ def create_simulation(parameter_values=None, experiment=None, make_inputs=False)
     """
     Create a PyBaMM simulation set up for interation with liionpack
 
-    Parameters
-    ----------
-    parameter_values : :class:`pybamm.ParameterValues`
-        The default is None.
-    experiment : :class:`pybamm.Experiment`
-        The default is None.
-    make_inputs : bool, optional
-        Changes "Current function [A]" and "Total heat transfer coefficient
-        [W.m-2.K-1]" to be inputs that are controlled by liionpack.
-        The default is False.
+    Args:
+        parameter_values (pybamm.ParameterValues):
+            The default is None.
+        experiment (pybamm.Experiment):
+            The default is None.
+        make_inputs (bool):
+            Changes "Current function [A]" and "Total heat transfer coefficient
+            [W.m-2.K-1]" to be inputs that are controlled by liionpack.
+            The default is False.
 
-    Returns
-    -------
-    sim : :class:`pybamm.Simulation`
-        A simulation that can be solved individually or passed into the
-        liionpack solve method
+    Returns:
+        sim (pybamm.Simulation):
+            A simulation that can be solved individually or passed into the
+            liionpack solve method
 
     """
     # Create the pybamm model
