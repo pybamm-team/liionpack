b8dd2e96d69e784848af31b69d8ce782a04162f1
Priyanshu Agarwal <priyanshuone6@gmail.com>
2021-11-21 14:36:40 +0530

    Add tests

---
+++ tests/unit/test_notebooks.py
@@ -0,0 +1,36 @@
+#
+# Tests jupyter notebooks
+#
+import os
+import sys
+import unittest
+
+import liionpack as lp
+
+
+class TestNotebooks(unittest.TestCase):
+    def test_notebooks(self):
+        examples_folder = os.path.join(lp.utils.ROOT_DIR, "examples", "notebooks")
+        for filename in os.listdir(examples_folder):
+            if os.path.splitext(filename)[1] == ".ipynb":
+                path = os.path.join(examples_folder, filename)
+                print("Testing notebook:", filename)
+
+            # Make sure the notebook has a pip install command, for using Google Colab
+            with open(path, "r") as f:
+                if (
+                    "!pip install -q git+https://github.com/pybamm-team/liionpack.git@main"  # noqa: E501
+                    not in f.read()
+                ):
+                    # Print error and exit
+                    print("\n" + "-" * 115)
+                    print("ERROR")
+                    print(
+                        "Installation command '!pip install -q git+https://github.com/pybamm-team/liionpack.git@main' not found in notebook"  # noqa: E501
+                    )
+                    print("-" * 115)
+                    return sys.exit(1)
+
+
+if __name__ == "__main__":
+    unittest.main()
