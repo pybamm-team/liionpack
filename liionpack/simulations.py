b9526ca509cb19b14833581176467336dc9dd76c
ksnvikrant <92038470+ksnvikrant@users.noreply.github.com>
2021-12-01 15:16:26 -0500

    Update simulations.py
    A SEI ec reaction limited degradation submodel is added.

--- liionpack/simulations.py
+++ liionpack/simulations.py
@@ -90,3 +90,41 @@ def thermal_simulation(parameter_values=None):
     )
 
     return sim
+def SEIdegradation_simulation(parameter_values=None):
+    """
+    Create a PyBaMM simulation set up for integration with liionpack
+
+    Args:
+        parameter_values (pybamm.ParameterValues):
+            The default is None.
+
+    Returns:
+        pybamm.Simulation:
+            A simulation that can be solved individually or passed into the
+            liionpack solve method
+
+    """
+    # Create the pybamm model
+    model = pybamm.lithium_ion.SPM(
+        options={
+            "SEI": "ec reaction limited","SEI film resistance": "distributed","SEI porosity change": "true",
+        }
+    )
+
+    # Add events to the model
+    model = lp.add_events_to_model(model)
+
+    # Set up parameter values
+    if parameter_values is None:
+        chemistry = pybamm.parameter_sets.Chen2020
+        parameter_values = pybamm.ParameterValues(chemistry=chemistry)
+
+    # Set up solver and simulation
+    solver = pybamm.CasadiSolver(mode="safe")
+    sim = pybamm.Simulation(
+        model=model,
+        parameter_values=parameter_values,
+        solver=solver,
+    )
+
+    return sim
