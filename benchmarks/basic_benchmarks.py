c4d648ff2698f00a86fb5217426bef101a185208
Tom Tranter <t.g.tranter@gmail.com>
2021-12-06 11:48:25 +0000

    style

--- benchmarks/basic_benchmarks.py
+++ benchmarks/basic_benchmarks.py
@@ -12,89 +12,116 @@ class BasicBenchmark:
     def time_solve_model(self):
         BasicBenchmark.sim.solve([0, 1800])
 
-class SmallPack():
+
+class SmallPack:
     def setup(self):
         self.netlist = lp.setup_circuit(Np=2, Ns=1, Rb=1e-4, Rc=1e-2)
         chemistry = pybamm.parameter_sets.Chen2020
         self.parameter_values = pybamm.ParameterValues(chemistry=chemistry)
-    
+
     def time_discharge_1cpu(self):
-        experiment = pybamm.Experiment([
-            "Discharge at 2 A for 5 minutes",
+        experiment = pybamm.Experiment(
+            [
+                "Discharge at 2 A for 5 minutes",
             ],
-            period="10 seconds")
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=1)
+            period="10 seconds",
+        )
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=1,
+        )
 
     def time_discharge_2cpu(self):
-        experiment = pybamm.Experiment([
-            "Discharge at 2 A for 5 minutes",
+        experiment = pybamm.Experiment(
+            [
+                "Discharge at 2 A for 5 minutes",
             ],
-            period="10 seconds")
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=2)
+            period="10 seconds",
+        )
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=2,
+        )
 
-class MediumPack():
+
+class MediumPack:
     def setup(self):
         self.netlist = lp.setup_circuit(Np=32, Ns=10, Rb=1e-4, Rc=1e-2)
         chemistry = pybamm.parameter_sets.Chen2020
         self.parameter_values = pybamm.ParameterValues(chemistry=chemistry)
-    
+
     def time_discharge_1cpu(self):
-        experiment = pybamm.Experiment([
-            "Discharge at 32 A for 5 minutes",
+        experiment = pybamm.Experiment(
+            [
+                "Discharge at 32 A for 5 minutes",
             ],
-            period="10 seconds")
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=1)
+            period="10 seconds",
+        )
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=1,
+        )
 
     def time_discharge_2cpu(self):
-        experiment = pybamm.Experiment([
-            "Discharge at 32 A for 5 minutes",
+        experiment = pybamm.Experiment(
+            [
+                "Discharge at 32 A for 5 minutes",
             ],
-            period="10 seconds")
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=2)
+            period="10 seconds",
+        )
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=2,
+        )
 
-class LargePack():
+
+class LargePack:
     def setup(self):
         self.netlist = lp.setup_circuit(Np=64, Ns=64, Rb=1e-4, Rc=1e-2)
         chemistry = pybamm.parameter_sets.Chen2020
         self.parameter_values = pybamm.ParameterValues(chemistry=chemistry)
-    
+
     def time_discharge_1cpu(self):
-        experiment = pybamm.Experiment([
-            "Discharge at 64 A for 5 minutes",
+        experiment = pybamm.Experiment(
+            [
+                "Discharge at 64 A for 5 minutes",
             ],
-            period="10 seconds")
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=1)
+            period="10 seconds",
+        )
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=1,
+        )
 
     def time_discharge_2cpu(self):
-        experiment = pybamm.Experiment([
-            "Discharge at 64 A for 5 minutes",
+        experiment = pybamm.Experiment(
+            [
+                "Discharge at 64 A for 5 minutes",
             ],
-            period="10 seconds")
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=2)
+            period="10 seconds",
+        )
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=2,
+        )
 
     def time_long_cycle_2cpu(self):
         I_app = 64
@@ -104,11 +131,14 @@ class LargePack():
                 "Rest for 15 minutes",
                 f"Discharge at {I_app} A for 20 minutes",
                 "Rest for 30 minutes",
-            ] * 10,
+            ]
+            * 10,
             period="10 seconds",
         )
-        _ = lp.solve(netlist=self.netlist.copy(),
-                     parameter_values=self.parameter_values.copy(),
-                     experiment=experiment,
-                     initial_soc=0.5,
-                     nproc=2)
+        _ = lp.solve(
+            netlist=self.netlist.copy(),
+            parameter_values=self.parameter_values.copy(),
+            experiment=experiment,
+            initial_soc=0.5,
+            nproc=2,
+        )
