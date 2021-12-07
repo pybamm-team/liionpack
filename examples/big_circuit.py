9438195053fad5e4d32338dbed78374b0afcd7f2
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-12-07 12:24:57 -0500

    Update big circuit example

--- examples/big_circuit.py
+++ examples/big_circuit.py
@@ -1,29 +1,22 @@
-#
-# Simulate a big circuit
-#
+"""
+Simulate a big circuit containing 384 battery cells.
+"""
 
 import liionpack as lp
 import pybamm
-import matplotlib.pyplot as plt
 import os
 
 if __name__ == "__main__":
 
-    plt.close("all")
     lp.logger.setLevel("NOTICE")
 
     Np = 32
     Ns = 12
     Nspm = Np * Ns
-    I_app = Np * 2.0
+    I_app = 5
 
     # Generate the netlist
-    netlist = lp.setup_circuit(Np=Np, Ns=Ns, Rb=1e-4, Rc=1e-2, Ri=5e-2, V=3.2, I=I_app)
-
-    output_variables = [
-        "X-averaged negative particle surface concentration [mol.m-3]",
-        "X-averaged positive particle surface concentration [mol.m-3]",
-    ]
+    netlist = lp.setup_circuit(Np=Np, Ns=Ns, I=I_app)
 
     # Cycling experiment
     experiment = pybamm.Experiment(
@@ -45,8 +38,8 @@ if __name__ == "__main__":
         netlist=netlist,
         parameter_values=parameter_values,
         experiment=experiment,
-        output_variables=output_variables,
         nproc=os.cpu_count(),
     )
 
     lp.plot_output(output)
+    lp.show_plots()
