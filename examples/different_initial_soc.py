13cb0231cfe22031031bdc9d1e30e306ec4d8be9
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-12-07 13:43:58 -0500

    Update example of different SOC

--- examples/different_initial_soc.py
+++ examples/different_initial_soc.py
@@ -1,53 +1,45 @@
-#
-# Simulation with two batteries of different initial soc
-#
+"""
+Example of running a simulation with two batteries of different initial SOC.
+"""
 
 import liionpack as lp
 import pybamm
-import matplotlib.pyplot as plt
 import numpy as np
 
-plt.close("all")
 lp.logger.setLevel("NOTICE")
 
-# Generate the netlist
-netlist = lp.setup_circuit(Np=2, Ns=1, Rb=1.5e-3, Rc=1e-2, Ri=5e-2, V=4.0, I=5.0)
+# Define parameters
+Np = 2
+Ns = 1
+Iapp = 5
+
+# Generate the netlist and output variables
+netlist = lp.setup_circuit(Np=Np, Ns=Ns, Rb=1.5e-3, Rc=1e-2, Ri=5e-2, V=4.0, I=Iapp)
 output_variables = [
     "X-averaged negative particle surface concentration [mol.m-3]",
-    "X-averaged positive particle surface concentration [mol.m-3]",
-]
+    "X-averaged positive particle surface concentration [mol.m-3]"]
 
-I_app = 2.0
 # Cycling experiment
-experiment = pybamm.Experiment(
-    [
-        f"Charge at {I_app} A for 30 minutes",
-        "Rest for 15 minutes",
-        f"Discharge at {I_app} A for 30 minutes",
-        "Rest for 30 minutes",
-    ],
-    period="10 seconds",
-)
+experiment = pybamm.Experiment([
+    f"Charge at {Iapp} A for 30 minutes",
+    "Rest for 15 minutes",
+    f"Discharge at {Iapp} A for 30 minutes",
+    "Rest for 30 minutes"],
+    period="10 seconds")
 
 # PyBaMM parameters
 chemistry = pybamm.parameter_sets.Chen2020
 param = pybamm.ParameterValues(chemistry=chemistry)
 
-c_s_n_init, c_s_p_init = lp.update_init_conc(
-    param, SoC=np.array([0.5, 0.6]), update=False
-)
+c_s_n_init, c_s_p_init = lp.update_init_conc(param, SoC=np.array([0.5, 0.6]), update=False)
 
-param.update(
-    {
-        "Initial concentration in negative electrode [mol.m-3]": "[input]",
-        "Initial concentration in positive electrode [mol.m-3]": "[input]",
-    }
-)
+param.update({
+    "Initial concentration in negative electrode [mol.m-3]": "[input]",
+    "Initial concentration in positive electrode [mol.m-3]": "[input]"})
 
 inputs = {
     "Initial concentration in negative electrode [mol.m-3]": c_s_n_init,
-    "Initial concentration in positive electrode [mol.m-3]": c_s_p_init,
-}
+    "Initial concentration in positive electrode [mol.m-3]": c_s_p_init}
 
 # Solve pack
 output = lp.solve(
@@ -56,7 +48,8 @@ output = lp.solve(
     experiment=experiment,
     output_variables=output_variables,
     initial_soc=None,
-    inputs=inputs,
-)
+    inputs=inputs)
 
+# Plot results
 lp.plot_output(output)
+lp.show_plots()
