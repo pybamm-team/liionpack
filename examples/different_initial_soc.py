3e38fe3e387377cde74c13c7ba086a3bc694d055
Tom Tranter <t.g.tranter@gmail.com>
2021-12-13 15:52:08 +0000

    Style and remove load_netlist example

--- examples/different_initial_soc.py
+++ examples/different_initial_soc.py
@@ -17,29 +17,39 @@ Iapp = 5
 netlist = lp.setup_circuit(Np=Np, Ns=Ns, Rb=1.5e-3, Rc=1e-2, Ri=5e-2, V=4.0, I=Iapp)
 output_variables = [
     "X-averaged negative particle surface concentration [mol.m-3]",
-    "X-averaged positive particle surface concentration [mol.m-3]"]
+    "X-averaged positive particle surface concentration [mol.m-3]",
+]
 
 # Cycling experiment
-experiment = pybamm.Experiment([
-    f"Charge at {Iapp} A for 30 minutes",
-    "Rest for 15 minutes",
-    f"Discharge at {Iapp} A for 30 minutes",
-    "Rest for 30 minutes"],
-    period="10 seconds")
+experiment = pybamm.Experiment(
+    [
+        f"Charge at {Iapp} A for 30 minutes",
+        "Rest for 15 minutes",
+        f"Discharge at {Iapp} A for 30 minutes",
+        "Rest for 30 minutes",
+    ],
+    period="10 seconds",
+)
 
 # PyBaMM parameters
 chemistry = pybamm.parameter_sets.Chen2020
 param = pybamm.ParameterValues(chemistry=chemistry)
 
-c_s_n_init, c_s_p_init = lp.update_init_conc(param, SoC=np.array([0.5, 0.6]), update=False)
+c_s_n_init, c_s_p_init = lp.update_init_conc(
+    param, SoC=np.array([0.5, 0.6]), update=False
+)
 
-param.update({
-    "Initial concentration in negative electrode [mol.m-3]": "[input]",
-    "Initial concentration in positive electrode [mol.m-3]": "[input]"})
+param.update(
+    {
+        "Initial concentration in negative electrode [mol.m-3]": "[input]",
+        "Initial concentration in positive electrode [mol.m-3]": "[input]",
+    }
+)
 
 inputs = {
     "Initial concentration in negative electrode [mol.m-3]": c_s_n_init,
-    "Initial concentration in positive electrode [mol.m-3]": c_s_p_init}
+    "Initial concentration in positive electrode [mol.m-3]": c_s_p_init,
+}
 
 # Solve pack
 output = lp.solve(
@@ -48,7 +58,8 @@ output = lp.solve(
     experiment=experiment,
     output_variables=output_variables,
     initial_soc=None,
-    inputs=inputs)
+    inputs=inputs,
+)
 
 # Plot results
 lp.plot_output(output)
