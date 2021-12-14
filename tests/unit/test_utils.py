49489fa9e4593370612c0fdca072d8ccb36199f8
Tom Tranter <t.g.tranter@gmail.com>
2021-12-14 09:54:03 +0000

    Remove data tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,5 @@
 import liionpack as lp
 import pandas as pd
-import numpy as np
 import pybamm
 import unittest
 
@@ -11,22 +10,6 @@ class utilsTest(unittest.TestCase):
         df = pd.DataFrame(data=d)
         f = lp.interp_current(df)
         assert f(5) == 3.0
-
-    def test_read_cfd_data_linear(self):
-        data, xv, yv, planes = lp.read_cfd_data()
-        expected = np.array([8.295482896342516, 7.878096887315587])
-        T = np.ones(32) * xv[1, 1]
-        Q = yv[1, 1]
-        htc = lp.get_linear_htc(planes, T, Q)
-        assert np.allclose(htc[:2], expected)
-
-    def test_read_cfd_data_interpolated(self):
-        data, xv, yv, funcs = lp.read_cfd_data(fit="interpolated")
-        expected = np.array([8.295482896342516, 7.878096887315587])
-        T = np.ones(32) * xv[1, 1]
-        Q = yv[1, 1]
-        htc = lp.get_interpolated_htc(funcs, T, Q)
-        assert np.allclose(htc[:2], expected)
 
     def test_add_events_to_model(self):
         model = pybamm.lithium_ion.SPMe()
